86446aef3df623e185364a92fd9716a038bff0e2
dGrammatiko <d.grammatiko@gmail.com>
2020-08-20 16:25:03 +0200

    meh

--- functions/vote.js
+++ functions/vote.js
@@ -1,11 +1,44 @@
-exports.handler = (event, context, callback) => {
+import querystring from "querystring";
+import fetch from "node-fetch";
+
+const API_ENDPOINT = "https://icanhazdadjoke.com/";
+
+// const { FaunaKey } = process.env;
+
+exports.handler = async (event, context) => {
     console.log(JSON.stringify(event))
     console.log(JSON.stringify(context))
-
-    if (event.httpMethod.toUpperCase() !== 'POST' || !event.body) {
-        callback(null, {
-            statusCode: 500,
-            message: 'Server Error',
-        });
+    // Only allow POST
+    if (event.httpMethod !== "POST" || !event.body) {
+        return { statusCode: 405, body: "Method Not Allowed" };
     }
-}
+
+    // When the method is POST, the name will no longer be in the event’s
+    // queryStringParameters – it’ll be in the event body encoded as a query string
+    const params = querystring.parse(event.body);
+    const url = params.url || false;
+    const userIp = params.userIp || false;
+    const value = params.value || false;
+
+    if (!url || !userIp || !value) {
+        return { statusCode: 405, body: "Missing arguments" };
+    }
+
+    // {
+    //     url: location.href,
+    //         userIp: window.currentIp.ip,
+    //             value: this.value
+    // }
+    // return fetch(API_ENDPOINT, { headers: { "Accept": "application/json" } })
+    //     .then(response => response.json())
+    //     .then(data => ({
+    //         statusCode: 200,
+    //         body: data.joke
+    //     }))
+    //     .catch(error => ({ statusCode: 422, body: String(error) }));
+
+    return {
+        statusCode: 200,
+        body: `Hello, url: ${url} , userIp: ${userIp} , value: ${values}`
+    };
+};
