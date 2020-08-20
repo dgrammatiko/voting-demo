3bc0ef5b31e07c7097f515a11f669650e44627af
dGrammatiko <d.grammatiko@gmail.com>
2020-08-20 16:45:38 +0200

    meh

--- functions/vote.js
+++ functions/vote.js
@@ -15,7 +15,7 @@ exports.handler = async (event, context) => {
 
     // When the method is POST, the name will no longer be in the event’s
     // queryStringParameters – it’ll be in the event body encoded as a query string
-    const params = querystring.parse(event.body);
+    const params = JSON.parse(event.body);
     const url = params.url || false;
     const userIp = params.userIp || false;
     const value = params.value || false;
