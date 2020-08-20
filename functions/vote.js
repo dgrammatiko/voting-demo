d38733cb6f1a2e72a6fddc7d8d88ace00c643042
dGrammatiko <d.grammatiko@gmail.com>
2020-08-20 17:38:27 +0200

    meh

--- functions/vote.js
+++ functions/vote.js
@@ -1,21 +1,21 @@
-const querystring = require("querystring");
-const fetch = require("node-fetch");
+const faunadb = require('faunadb');
+const q = faunadb.query;
 
-const API_ENDPOINT = "https://icanhazdadjoke.com/";
-
-// const { FaunaKey } = process.env;
+const client = new faunadb.Client({
+    secret: process.env.FAUNADB_SECRET
+});
 
 exports.handler = async (event, context) => {
-    console.log(JSON.stringify(event))
-    console.log(JSON.stringify(context))
     // Only allow POST
     if (event.httpMethod !== "POST" || !event.body) {
         return { statusCode: 405, body: "Method Not Allowed" };
     }
 
-    // When the method is POST, the name will no longer be in the event’s
-    // queryStringParameters – it’ll be in the event body encoded as a query string
-    console.dir(event.body)
+    // Make sure we have the needed env variables
+    if (!FaunaKey || !FaunaUrl) {
+        return { statusCode: 405, body: "Misconfigured Server" };
+    }
+
     const params = JSON.parse(event.body);
     const url = params.url || false;
     const userIp = params.userIp || false;
@@ -25,18 +25,19 @@ exports.handler = async (event, context) => {
         return { statusCode: 405, body: "Missing arguments" };
     }
 
-    // {
-    //     url: location.href,
-    //         userIp: window.currentIp.ip,
-    //             value: this.value
-    // }
-    // return fetch(API_ENDPOINT, { headers: { "Accept": "application/json" } })
-    //     .then(response => response.json())
-    //     .then(data => ({
-    //         statusCode: 200,
-    //         body: data.joke
-    //     }))
-    //     .catch(error => ({ statusCode: 422, body: String(error) }));
+    let data;
+    try {
+        data = await client.query(q.Paginate(q.Match(q.Ref(`demo/${url}`))));
+    } catch (e) {
+        return { statusCode: 500, body: "Misconfigured DB" };
+    }
+
+    if (!data) {
+        return { statusCode: 500, body: "No data" };
+    } else {
+        return { statusCode: 200, body: JSON.stringify(data) };
+    }
+
 
     return {
         statusCode: 200,
